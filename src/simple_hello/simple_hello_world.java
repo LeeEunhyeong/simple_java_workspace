f1704cee38d6db4b2c3e11944f3a930a468e92d6
LeeEunhyeong <lehyong03@naver.com>
2016-06-30 11:19:23 +0900

    new file

---
+++ src/simple_hello/simple_hello_world.java
@@ -0,0 +1,11 @@
+package simple_hello;
+
+public class simple_hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+}
